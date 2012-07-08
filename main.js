ab766af16a03c6eb9b8777a248b5468b91f9fb7d
Joel Klabo <joelklabo@gmail.com>
2012-07-07 19:11:29 -0700

    had opposite truthiness

--- main.js
+++ main.js
@@ -8,18 +8,22 @@ var isIgnored = function (root, ignoreDirs) {
   if (!ignoreDirs.length) { return false }
   ignoreDirs.forEach(function (dir) {
     if (root.indexOf(dir) > -1) {
-      return true
+      return false 
     }
   })
-  return false
+  return true 
 }
 
 var find = function (name, opts, cb) {
   if (!cb) {cb = opts; opts = {}}
   if (!opts.ignore) {opts.ignore = []}
-  var ignoreDirs = opts.ignore
+  if (!opts.dir) {opts.dir = cwd}
 
-  walker = walk.walk(cwd);
+  var ignoreDirs = opts.ignore
+    , startDir = opts.dir
+    ;
+
+  walker = walk.walk(startDir);
   walker.on('directories', function (root, dirs, next) {
     dirs.forEach(function (dir) {
       if (!!isIgnored(root, ignoreDirs) && dir.name == name) {
